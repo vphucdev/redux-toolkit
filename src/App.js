b82f37279f0eef87232f7acd88ab1e3186115692
“phucdev” <vanphuc0295@gmail.com>
2023-08-16 09:54:09 +0700

    install redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Edit from "./Components/Edit/Edit";
 
 function App() {
+  const [edit, setEdit] = useState(false);
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App">{edit ? <Edit setEdit={setEdit} /> :  <Header setEdit={setEdit} />}</div>
   );
 }
 
