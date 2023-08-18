978bb6599ce197668ac2c34f9e5b333fc4c98222
“phucdev” <vanphuc0295@gmail.com>
2023-08-18 09:40:57 +0700

    base

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,27 @@
+import { useSelector } from "react-redux";
 import "./Header.css";
 function Header({ setEdit }) {
-    const handleEdit = () => {
-        setEdit(true);
-    }
+  const handleEdit = () => {
+    setEdit(true);
+  };
+  const user = useSelector((state) => state.user);
+
   return (
     <>
-      <header style={{backgroundColor: '#ff9051', backgroundImage: 'linear-gradient(180deg, #ff9051 2%, #ff9051, 65%, #181818 100%)'}} >
+      <header
+        style={{
+          backgroundColor: `${user.themeColor}`,
+          backgroundImage: `linear-gradient(180deg, ${user.themeColor} 2%, ${user.themeColor}, 65%, #181818 100%)`,
+        }}
+      >
         <div className="info-container">
-          <div className="info-edit" onClick={handleEdit}>Edit</div>
-          <img className="info-ava" src="https://preview.redd.it/rrz3hmsxcll71.png?width=640&crop=smart&auto=webp&s=87cc5ed38d8f088ef9fffef7a4c5756b64309d6a" alt="" />
-          <div className="info-username">Phuc</div>
-          <div className="info-age">20</div>
-          <div className="info-about">about</div>
+          <div className="info-edit" onClick={handleEdit}>
+            Edit
+          </div>
+          <img className="info-ava" src={user.avaUrl} alt="" />
+          <div className="info-username">{user.name}</div>
+          <div className="info-age">{user.age}</div>
+          <div className="info-about">{user.about}</div>
         </div>
       </header>
     </>
