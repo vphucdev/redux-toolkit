6575454d6808af04f951a018237bbd93e0d44ba7
“phucdev” <vanphuc0295@gmail.com>
2023-08-22 15:38:54 +0700

    add cpn MakePost, Footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Edit from "./Components/Edit/Edit";
+import { useSelector } from "react-redux";
+import Footer from "./Components/Footer/Footer";
+import MakePost from "./Components/Posts/MakePost";
 
 function App() {
   const [edit, setEdit] = useState(false);
+  const [openPost, setOpenPost] = useState(false);
+
+  const pending = useSelector((state) => state.user.pending);
+  const error = useSelector((state) => state.user.error);
   return (
-    <div className="App">{edit ? <Edit setEdit={setEdit} /> :  <Header setEdit={setEdit} />}</div>
+    <div className="App">
+      {edit ? <Edit setEdit={setEdit} /> : <Header setEdit={setEdit} />}
+      {pending && <p className="loading">Loading...</p>}
+      {!edit && error && <p className="error">Error when fetching data</p>}
+      {openPost && <MakePost /> }
+
+      <Footer openPost={openPost} setOpenPost={setOpenPost} />
+    </div>
   );
 }
 
