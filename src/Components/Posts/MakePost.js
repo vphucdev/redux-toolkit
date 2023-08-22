c3c839d29b1dd7809d1dab407594999034612993
“phucdev” <vanphuc0295@gmail.com>
2023-08-22 16:25:28 +0700

    add input, tag name

--- src/Components/Posts/MakePost.js
+++ src/Components/Posts/MakePost.js
@@ -1,15 +1,48 @@
 import { useState } from "react";
 import Input from "../Input/Input";
-import './Post.css'
+import "./Post.css";
 
 function MakePost() {
-const [title, setTitle] = useState('add a title')
+  const [title, setTitle] = useState("add a title");
+  const [desc, setDesc] = useState("add some description");
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const tags = ["None", "NSFW", "Mood", "Qoutes", "Shitpost"];
   return (
     <section className="makepost-container">
       <div className="makepost-navigation">
-        <p className="makepost-save">post</p>
-          </div>
-          <Input title='Title' type='textarea' className='makepost-title' placeholder={title} handleOnChange={setTitle} />
+        <p className="makepost-save">Post</p>
+      </div>
+      <Input
+        title="Title"
+        type="textarea"
+        className="makepost-title"
+        placeholder={title}
+        handleOnChange={setTitle}
+      />
+      <Input
+        title="Descriptions"
+        type="textarea"
+        className="makepost-desc"
+        placeholder={desc}
+        handleOnChange={setDesc}
+      />
+      <label> Tags</label>
+      <div className="makepost-tags">
+        {tags.map((tag, index) => (
+          <button
+            key={index}
+            className={
+              selectedIndex === index
+                ? "makepost-tags-selected"
+                : `makepost-tags-${tag}`
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
+            {tag}
+          </button>
+        ))}
+      </div>
     </section>
   );
 }
