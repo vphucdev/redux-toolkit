ad3651c3a0c30f7542515c8027cb595b0e118666
“phucdev” <vanphuc0295@gmail.com>
2023-08-22 15:41:25 +0700

    update: start, error,success

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -7,21 +7,26 @@ export const userSlice = createSlice({
     about: "about",
     avaUrl:
       "https://preview.redd.it/rrz3hmsxcll71.png?width=640&crop=smart&auto=webp&s=87cc5ed38d8f088ef9fffef7a4c5756b64309d6a",
-    themeColor: '#ff9051',
-  pending: false,
-  error: false,
+    themeColor: "#ff9051",
+    pending: false,
+    error: false,
   },
   reducers: {
-    updateStart: (state) => { 
-      state.pending =true
+    updateStart: (state) => {
+      state.pending = true;
     },
-    updateError: (state) => { 
-      state.pending = false
-      state.error = true
-      
+    updateError: (state) => {
+      state.pending = false;
+      state.error = true;
     },
-    updateSuccess: (state, action) => { 
-      state.pending =false
+    updateSuccess: (state, action) => {
+      state.pending = false;
+      state.error = false;
+      state.name = action.payload.name;
+      state.age = action.payload.age;
+      state.about = action.payload.about;
+      state.avaUrl = action.payload.avaUrl;
+      state.themeColor = action.payload.themeColor;
     },
     // update: (state, action) => {
     //   state.name = action.payload.name;
@@ -29,9 +34,9 @@ export const userSlice = createSlice({
     //   state.about = action.payload.about;
     //   state.avaUrl = action.payload.avaUrl;
     //   state.themeColor = action.payload.themeColor;
-    // },
+    // }
   },
 });
 
-export const {update} = userSlice.actions
-export default userSlice.reducer //import bên store.js
+export const {/*update,*/updateSuccess, updateError, updateStart } = userSlice.actions;
+export default userSlice.reducer; //import bên store.js
